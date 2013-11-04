83338b25a07b57a64327e8d950f73763ea7245cd
刘凯 <kai8406@gmail.com>
2013-11-04 15:00:12 +0800

    增加CMDBuild数据库中的一些配置常量.

---
+++ sobey-projects/cmdbuild/src/main/java/com/sobey/cmdbuild/utils/CMDBuildConstants.java
@@ -0,0 +1,26 @@
+package com.sobey.cmdbuild.utils;
+
+/**
+ * CMDBuild数据库中的一些配置常量.
+ * 
+ * @author Administrator
+ * 
+ */
+public class CMDBuildConstants {
+
+	/**
+	 * "A" 活跃
+	 */
+	public static final Character STATUS_ACTIVE = 'A';
+
+	/**
+	 * "U" 更新
+	 */
+	public static final Character STATUS_UPDATE = 'U';
+
+	/**
+	 * "N" 非活跃
+	 */
+	public static final Character STATUS_NON_ACTIVE = 'N';
+
+}
