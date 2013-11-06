b8f93ced2dc18bedeb1ea6f7d25e3efea4951cd0
刘凯 <kai8406@gmail.com>
2013-11-06 15:56:18 +0800

    其他的soapservice都整合进cmdbuildsoapservice中.

--- sobey-projects/cmdbuild/src/main/java/com/sobey/cmdbuild/webservice/CmdbuildSoapService.java
+++ sobey-projects/cmdbuild/src/main/java/com/sobey/cmdbuild/webservice/CmdbuildSoapService.java
@@ -7,19 +7,24 @@ import javax.jws.WebService;
 
 import com.sobey.cmdbuild.constants.WsConstants;
 import com.sobey.cmdbuild.webservice.response.dto.CompanyDTO;
+import com.sobey.cmdbuild.webservice.response.dto.TenantsDTO;
 import com.sobey.cmdbuild.webservice.response.result.DTOListResult;
 import com.sobey.cmdbuild.webservice.response.result.DTOResult;
 import com.sobey.cmdbuild.webservice.response.result.IdResult;
 import com.sobey.cmdbuild.webservice.response.result.PaginationResult;
 
 /**
- * CMDBuild模块对外暴露的webservice接口.
+ * CMDBuild模块对外暴露的唯一的webservice接口.
  * 
  * @author Administrator
  * 
  */
 @WebService(name = "CmdbuildService", targetNamespace = WsConstants.NS)
 public interface CmdbuildSoapService {
+
+	/*************************************************
+	 *************** Organisation ********************
+	 *************************************************/
 
 	// ==============================//
 	// =========== Comany ===========//
@@ -40,5 +45,102 @@ public interface CmdbuildSoapService {
 	PaginationResult<CompanyDTO> getCompanyPagination(
 			@WebParam(name = "searchParams") Map<String, Object> searchParams,
 			@WebParam(name = "pageNumber") Integer pageNumber, @WebParam(name = "pageSize") Integer pageSize);
+
+	// ==============================//
+	// =========== Tenants ==========//
+	// ==============================//
+
+	DTOResult<TenantsDTO> findTenants(@WebParam(name = "id") Integer id);
+
+	DTOResult<TenantsDTO> findTenantsByCode(@WebParam(name = "code") String code);
+
+	IdResult createTenants(@WebParam(name = "tenantsDTO") TenantsDTO tenantsDTO);
+
+	IdResult updateTenants(@WebParam(name = "id") Integer id, @WebParam(name = "tenantsDTO") TenantsDTO tenantsDTO);
+
+	IdResult deleteTenants(@WebParam(name = "id") Integer id);
+
+	DTOListResult<TenantsDTO> getTenantsList();
+
+	PaginationResult<TenantsDTO> getTenantsPagination(
+			@WebParam(name = "searchParams") Map<String, Object> searchParams,
+			@WebParam(name = "pageNumber") Integer pageNumber, @WebParam(name = "pageSize") Integer pageSize);
+
+	// ==============================//
+	// ============ IDC =============//
+	// ==============================//
+
+	// DTOResult<IdcDTO> findIdc(@WebParam(name = "id") Integer id);
+	//
+	// DTOResult<IdcDTO> findIdcByCode(@WebParam(name = "code") String code);
+	//
+	// IdResult createIdc(@WebParam(name = "idcDTO") IdcDTO idcDTO);
+	//
+	// IdResult updateIdc(@WebParam(name = "id") Integer id, @WebParam(name = "idcDTO") IdcDTO idcDTO);
+	//
+	// IdResult deleteIdc(@WebParam(name = "id") Integer id);
+	//
+	// DTOListResult<IdcDTO> getIdcList();
+	//
+	// PaginationResult<IdcDTO> getIdcPagination(@WebParam(name = "searchParams") Map<String, Object> searchParams,
+	// @WebParam(name = "pageNumber") Integer pageNumber, @WebParam(name = "pageSize") Integer pageSize);
+	//
+	// // ==============================//
+	// // ============ Rack ============//
+	// // ==============================//
+	//
+	// DTOResult<RackDTO> findRack(@WebParam(name = "id") Integer id);
+	//
+	// DTOResult<RackDTO> findRackByCode(@WebParam(name = "code") String code);
+	//
+	// IdResult createRack(@WebParam(name = "rackDTO") RackDTO rackDTO);
+	//
+	// IdResult updateRack(@WebParam(name = "id") Integer id, @WebParam(name = "rackDTO") RackDTO rackDTO);
+	//
+	// IdResult deleteRack(@WebParam(name = "id") Integer id);
+	//
+	// DTOListResult<RackDTO> getRackList();
+	//
+	// PaginationResult<RackDTO> getRackPagination(@WebParam(name = "searchParams") Map<String, Object> searchParams,
+	// @WebParam(name = "pageNumber") Integer pageNumber, @WebParam(name = "pageSize") Integer pageSize);
+	//
+	// // ==============================//
+	// // ============ Tag =============//
+	// // ==============================//
+	//
+	// DTOResult<TagDTO> findTag(@WebParam(name = "id") Integer id);
+	//
+	// DTOResult<TagDTO> findTagByCode(@WebParam(name = "code") String code);
+	//
+	// IdResult createTag(@WebParam(name = "tagDTO") TagDTO tagDTO);
+	//
+	// IdResult updateTag(@WebParam(name = "id") Integer id, @WebParam(name = "tagDTO") TagDTO tagDTO);
+	//
+	// IdResult deleteTag(@WebParam(name = "id") Integer id);
+	//
+	// DTOListResult<TagDTO> getTagList();
+	//
+	// PaginationResult<TagDTO> getTagPagination(@WebParam(name = "searchParams") Map<String, Object> searchParams,
+	// @WebParam(name = "pageNumber") Integer pageNumber, @WebParam(name = "pageSize") Integer pageSize);
+
+	/*************************************************
+	 ****************** Financial ********************
+	 *************************************************/
+
+	// ==============================//
+	// ========= Consumptions =======//
+	// ==============================//
+
+	// ==============================//
+	// ========= DeviceSpec =======//
+	// ==============================//
+
+	// ==============================//
+	// =========== EipSpec ==========//
+	// ==============================//
+
+	// ==============================//
+	// ============ Es3Spec =========//
+	// ==============================//
 
 }
