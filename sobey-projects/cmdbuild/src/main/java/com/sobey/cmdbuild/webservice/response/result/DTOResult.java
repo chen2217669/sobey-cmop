fba32785f6debdf5311f3ffd93ba215e3cd25b30
刘凯 <kai8406@gmail.com>
2013-11-06 13:58:57 +0800

    Add comment

--- sobey-projects/cmdbuild/src/main/java/com/sobey/cmdbuild/webservice/response/result/DTOResult.java
+++ sobey-projects/cmdbuild/src/main/java/com/sobey/cmdbuild/webservice/response/result/DTOResult.java
@@ -4,6 +4,13 @@ import javax.xml.bind.annotation.XmlType;
 
 import com.sobey.cmdbuild.constants.WsConstants;
 
+/**
+ * 针对单个DTO的返回
+ * 
+ * @author Administrator
+ * 
+ * @param <T>
+ */
 @XmlType(name = "DTOResult", namespace = WsConstants.NS)
 public class DTOResult<T> extends WSResult {
 
