38cb7c5a39922d8d185a91aecf66d76069533fef
刘凯 <kai8406@gmail.com>
2013-11-07 15:31:02 +0800

    add error message

--- sobey-projects/cmdbuild/src/main/java/com/sobey/cmdbuild/constants/ERROR.java
+++ sobey-projects/cmdbuild/src/main/java/com/sobey/cmdbuild/constants/ERROR.java
@@ -8,4 +8,5 @@ public class ERROR {
 	public static final String OBJECT_DUPLICATE = "对象存在唯一性冲突";
 	public static final String OBJECT_NULL = "对象不存在";
 	public static final String INPUT_NULL = "输入参数为空";
+	public static final String MORE_RESULT = "结果返回多个元素";
 }
