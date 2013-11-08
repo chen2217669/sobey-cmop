8bab58ad134a22a010abd323796185afdac896f5
刘凯 <kai8406@gmail.com>
2013-11-08 15:42:10 +0800

    增加LookUp相关的DTO和DAO

---
+++ sobey-projects/cmdbuild/src/main/java/com/sobey/cmdbuild/webservice/response/dto/LookUpDTO.java
@@ -0,0 +1,105 @@
+package com.sobey.cmdbuild.webservice.response.dto;
+
+import java.util.Date;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+import com.sobey.cmdbuild.constants.WsConstants;
+
+@XmlRootElement
+@XmlType(name = "LookUpDTO", namespace = WsConstants.NS)
+public class LookUpDTO {
+
+	private Integer id;
+	private String code;
+	private String description;
+	private Date beginDate;
+	private String type;
+	private String parentType;
+	private Integer parentId;
+	private Integer number;
+	private Boolean isDefault;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Date getBeginDate() {
+		return beginDate;
+	}
+
+	public void setBeginDate(Date beginDate) {
+		this.beginDate = beginDate;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getParentType() {
+		return parentType;
+	}
+
+	public void setParentType(String parentType) {
+		this.parentType = parentType;
+	}
+
+	public Integer getParentId() {
+		return parentId;
+	}
+
+	public void setParentId(Integer parentId) {
+		this.parentId = parentId;
+	}
+
+	public Integer getNumber() {
+		return number;
+	}
+
+	public void setNumber(Integer number) {
+		this.number = number;
+	}
+
+	public Boolean getIsDefault() {
+		return isDefault;
+	}
+
+	public void setIsDefault(Boolean isDefault) {
+		this.isDefault = isDefault;
+	}
+
+	/**
+	 * 重新实现toString()函数方便在日志中打印DTO信息.
+	 */
+	@Override
+	public String toString() {
+		return ToStringBuilder.reflectionToString(this);
+	}
+}
