aba84357b670c9b98d0c97b48b6e7da53de08141
刘凯 <kai8406@gmail.com>
2013-11-08 15:48:28 +0800

    add lookup junit

--- sobey-projects/cmdbuild/src/test/java/com/sobey/cmdbuild/webservice/LookUpSoapTest.java
+++ sobey-projects/cmdbuild/src/test/java/com/sobey/cmdbuild/webservice/LookUpSoapTest.java
@@ -17,9 +17,10 @@ import org.springframework.test.context.support.DependencyInjectionTestExecution
 
 import com.google.common.collect.Maps;
 import com.sobey.cmdbuild.BaseFunctionalTestCase;
+import com.sobey.cmdbuild.constants.LookUpEnum;
 import com.sobey.cmdbuild.data.TestData;
-import com.sobey.cmdbuild.entity.Company;
-import com.sobey.cmdbuild.webservice.response.dto.CompanyDTO;
+import com.sobey.cmdbuild.entity.LookUp;
+import com.sobey.cmdbuild.webservice.response.dto.LookUpDTO;
 import com.sobey.cmdbuild.webservice.response.result.DTOListResult;
 import com.sobey.cmdbuild.webservice.response.result.DTOResult;
 import com.sobey.cmdbuild.webservice.response.result.IdResult;
@@ -27,7 +28,7 @@ import com.sobey.cmdbuild.webservice.response.result.PaginationResult;
 import com.sobey.core.mapper.BeanMapper;
 
 /**
- * Company SOAP服务的功能测试, 测试主要的接口调用.
+ * LookUp SOAP服务的功能测试, 测试主要的接口调用.
  * 
  * 使用在Spring applicaitonContext.xml中用<jaxws:client/>，根据CmdbuildWebService接口创建的Client.
  * 
@@ -37,7 +38,7 @@ import com.sobey.core.mapper.BeanMapper;
 @RunWith(SpringJUnit4ClassRunner.class)
 @TestExecutionListeners({ DependencyInjectionTestExecutionListener.class })
 @ContextConfiguration(locations = { "/applicationContext-soap-client.xml" })
-public class CompanySoapTest extends BaseFunctionalTestCase {
+public class LookUpSoapTest extends BaseFunctionalTestCase {
 
 	@Autowired
 	private CmdbuildSoapService service;
@@ -45,53 +46,54 @@ public class CompanySoapTest extends BaseFunctionalTestCase {
 	@Test
 	@Ignore
 	public void find() {
-
-		Integer id = 85;
-		DTOResult<CompanyDTO> response = service.findCompany(id);
-		assertEquals("code9701", response.getDto().getCode());
+		Integer id = 70;
+		DTOResult<LookUpDTO> response = service.findLookUp(id);
+		assertEquals("CentOS 6.4 64bit", response.getDto().getDescription());
 
 		Map<String, Object> searchParams = Maps.newHashMap();
-		searchParams.put("EQ_zip", "zip");
-		DTOResult<CompanyDTO> responseParams = service.findCompanyByParams(searchParams);
-		assertEquals("code9701", responseParams.getDto().getCode());
+		searchParams.put("EQ_type", LookUpEnum.Image.name());
+		searchParams.put("EQ_description", "CentOS 6.4 64bit");
+
+		DTOResult<LookUpDTO> responseParams = service.findLookUpByParams(searchParams);
+		assertEquals("CentOS 6.4 64bit", responseParams.getDto().getDescription());
 	}
 
 	@Test
 	@Ignore
 	public void getList() {
 		Map<String, Object> searchParams = Maps.newHashMap();
-		DTOListResult<CompanyDTO> result = service.getCompanyList(searchParams);
+		searchParams.put("EQ_type", LookUpEnum.Image.name());
+
+		DTOListResult<LookUpDTO> result = service.getLookUpList(searchParams);
 		System.out.println("获得集合数量:" + result.getDtos().size());
 		assertEquals("0", result.getCode());
 	}
 
 	@Test
-	@Ignore
+	// @Ignore
 	public void save() {
-		Company company = TestData.randomCompany();
-		CompanyDTO companyDTO = BeanMapper.map(company, CompanyDTO.class);
-		IdResult response = service.createCompany(companyDTO);
+		LookUp LookUp = TestData.randomLookUp();
+		LookUpDTO LookUpDTO = BeanMapper.map(LookUp, LookUpDTO.class);
+		IdResult response = service.createLookUp(LookUpDTO);
 		assertNotNull(response.getId());
 	}
 
 	@Test
 	@Ignore
 	public void update() {
-
-		Integer id = 86;
-		DTOResult<CompanyDTO> response = service.findCompany(id);
-		CompanyDTO companyDTO = response.getDto();
-		companyDTO.setCode("coder");
-		companyDTO.setDescription("我是超人啊~!");
-		IdResult result = service.updateCompany(id, companyDTO);
+		Integer id = 70;
+		DTOResult<LookUpDTO> response = service.findLookUp(id);
+		LookUpDTO LookUpDTO = response.getDto();
+		LookUpDTO.setDescription("冬天来了啊");
+		IdResult result = service.updateLookUp(id, LookUpDTO);
 		assertNotNull(result.getId());
 	}
 
 	@Test
-	// @Ignore
+	@Ignore
 	public void delete() {
-		Integer id = 138;
-		IdResult response = service.deleteCompany(id);
+		Integer id = 70;
+		IdResult response = service.deleteTag(id);
 		assertNotNull(response.getId());
 	}
 
@@ -101,16 +103,9 @@ public class CompanySoapTest extends BaseFunctionalTestCase {
 
 		Map<String, Object> searchParams = new HashMap<String, Object>();
 
-		/**
-		 * TODO 查询有问题.设置 status = 'A' , 但是传递到下个方法后转换成了status = 65 , 存疑. <br>
-		 * 将该参数放置在最后一步的分页查询还是OK的.
-		 */
-		// searchParams.put("EQ_status", CMDBuildConstants.STATUS_ACTIVE);
-
-		PaginationResult<CompanyDTO> result = service.getCompanyPagination(searchParams, 1, 10);
+		PaginationResult<LookUpDTO> result = service.getLookUpPagination(searchParams, 2, 10);
 
 		assertNotNull(result.getGetTotalElements());
 		System.out.println("返回的查询结果数量:" + result.getGetTotalElements());
 	}
-
 }
