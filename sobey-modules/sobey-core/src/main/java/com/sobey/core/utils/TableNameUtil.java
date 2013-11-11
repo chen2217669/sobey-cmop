0af155c95b3d5ce9db1a3ec9f195f1abf5376b2f
刘凯 <kai8406@gmail.com>
2013-11-11 10:10:49 +0800

    完善注释

--- sobey-modules/sobey-core/src/main/java/com/sobey/core/utils/TableNameUtil.java
+++ sobey-modules/sobey-core/src/main/java/com/sobey/core/utils/TableNameUtil.java
@@ -2,6 +2,8 @@ package com.sobey.core.utils;
 
 import javax.persistence.Table;
 
+import org.apache.commons.lang3.StringUtils;
+
 /**
  * 获取实体对应的表名
  */
@@ -11,15 +13,12 @@ public class TableNameUtil {
 	 * 返回@Table中的name值，否则返回实体的名称
 	 * 
 	 * @param c
+	 *            Classm名称
 	 * @return String
 	 */
 	public static String getTableName(Class<?> c) {
 		String name = c.getAnnotation(Table.class).name();
-
-		if (name.equals(""))
-			return c.getSimpleName();
-
-		return name;
+		return StringUtils.isNotBlank(name) ? name : c.getSimpleName();
 	}
 
 }
